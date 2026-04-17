ac9ed6ee1cad4f7766ddbbf266085ed4743ad944
Nguyen Huu Nghia <nguyenhuunghia03052004@gmail.com>
2026-04-17 08:29:52 +0700

    giao diện quản lí khách hàng

--- asp/Controllers/CustomerController.cs
+++ asp/Controllers/CustomerController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 using asp.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace asp.Controllers
 {
@@ -10,19 +12,31 @@ namespace asp.Controllers
     {
         private readonly IMongoCollection<Customer> _customerCollection;
 
+        // Kết nối vào collection "Customers" trong MongoDB
         public CustomerController(IMongoDatabase database)
         {
             _customerCollection = database.GetCollection<Customer>("Customers");
         }
 
-        // Lấy danh sách khách hàng (Module 8.2)
+        // 1. Lấy danh sách toàn bộ khách hàng
         [HttpGet]
-        public async Task<List<Customer>> Get() => await _customerCollection.Find(_ => true).ToListAsync();
-
-        // Thêm khách hàng mới (Module 5.2.1)
-        [HttpPost]
-        public async Task<IActionResult> Post(Customer customer)
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
         {
+            var list = await _customerCollection.Find(_ => true)
+                .SortByDescending(x => x.Id)
+                .ToListAsync();
+            return Ok(list);
+        }
+
+        // 2. Thêm khách hàng mới (Pass Test Case: Bắt lỗi bỏ trống tên/sđt)
+        [HttpPost]
+        public async Task<ActionResult<Customer>> CreateCustomer(Customer customer)
+        {
+            if (string.IsNullOrEmpty(customer.Name) || string.IsNullOrEmpty(customer.Phone))
+            {
+                return BadRequest(new { message = "Tên và Số điện thoại là bắt buộc!" });
+            }
+
             await _customerCollection.InsertOneAsync(customer);
             return Ok(customer);
         }
