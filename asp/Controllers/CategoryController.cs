54c5d02d1cef2022450c44f98fcf3cda4d5b0306
Nguyen Huu Nghia <nguyenhuunghia03052004@gmail.com>
2026-04-19 02:23:35 +0700

    cập nhật Menu ADMIN

--- asp/Controllers/CategoryController.cs
+++ asp/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
-﻿using asp.Data;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
+using asp.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace asp.Controllers
 {
@@ -10,55 +12,22 @@ namespace asp.Controllers
     {
         private readonly IMongoCollection<Category> _categoryCollection;
 
-        public CategoryController(IMongoClient mongoClient)
+        public CategoryController(IMongoDatabase database)
         {
-            // Thay "BdsDB" bằng tên database thực tế của bạn
-            var database = mongoClient.GetDatabase("BdsDB");
             _categoryCollection = database.GetCollection<Category>("Categories");
         }
 
-        [cite_start]// GET: api/Category [cite: 250]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Category>>> GetAll()
         {
-            var categories = await _categoryCollection.Find(_ => true).ToListAsync();
-            return Ok(categories);
+            return Ok(await _categoryCollection.Find(_ => true).ToListAsync());
         }
 
-        [cite_start]// POST: api/Category [cite: 251]
         [HttpPost]
-        public async Task<ActionResult> Create(Category category)
+        public async Task<IActionResult> Create(Category category)
         {
             await _categoryCollection.InsertOneAsync(category);
             return Ok(new { message = "Thêm danh mục thành công!", data = category });
-        }
-
-        [cite_start]// PUT: api/Category/{id} [cite: 252]
-        [HttpPut("{id}")]
-        public async Task<ActionResult> Update(string id, Category updatedCategory)
-        {
-            var result = await _categoryCollection.ReplaceOneAsync(c => c.Id == id, updatedCategory);
-
-            if (result.MatchedCount == 0)
-            {
-                return NotFound(new { message = "Không tìm thấy danh mục" });
-            }
-
-            return Ok(new { message = "Cập nhật thành công!" });
-        }
-
-        // DELETE: api/Category/{id}
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(string id)
-        {
-            var result = await _categoryCollection.DeleteOneAsync(c => c.Id == id);
-
-            if (result.DeletedCount == 0)
-            {
-                return NotFound(new { message = "Không tìm thấy danh mục để xóa" });
-            }
-
-            return Ok(new { message = "Xóa danh mục thành công!" });
         }
     }
 }
--- asp/Data/Category.cs
+++ asp/Data/Category.cs
@@ -9,15 +9,7 @@ namespace asp.Data
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
 
-        [BsonElement("Name")]
         public string Name { get; set; } = null!;
-
         public string? Description { get; set; }
-
-        public string? Slug { get; set; }
-
-        public string? Icon { get; set; }
-
-        public int DisplayOrder { get; set; }
     }
 }
